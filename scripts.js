5eb78735bc85da23de75a269472d3ac00e7a41d8
Bobby Brice <bobby.brice@gmail.com>
2020-04-15 20:24:00 -0400

    added a data attribute to randomize the cards with some of the functionality being added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,54 @@
 const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+
+//Match Card
+//When we click the first card, it needs to wait until another card is flipped so you need vars to managed the flip state. In case there is no card flipped
 
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if(!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
 }
+
+function checkForMatch() {
+  //use conditional statement to check if the dataset value matches
+  //otherwise, trigger unFlip() Fn
+  if(firstCard.dataset.image === secondCard.dataset.image) {
+    disableCards();
+    return;
+  }
+  unFlipCards();
+}
+
+function disableCards() {
+  
+}
+
+unFlipCards() {
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
