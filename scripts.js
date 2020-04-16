02122ad08133d0df787ff3c18a33d966c1530299
Bobby Brice <bobby.brice@gmail.com>
2020-04-15 20:50:00 -0400

    added  a board reset and a shuffle function using the flex order property and math.random

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard;
 let secondCard;
 
@@ -7,35 +8,55 @@ let secondCard;
 //When we click the first card, it needs to wait until another card is flipped so you need vars to managed the flip state. In case there is no card flipped
 
 function flipCard() {
+  if(lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.add('flip');
 
   if(!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+
     return;
   }
   secondCard = this;
-  hasFlippedCard = false;
-
   checkForMatch();
 }
 
 function checkForMatch() {
   //use conditional statement to check if the dataset value matches
   //otherwise, trigger unFlip() Fn
-  if(firstCard.dataset.image === secondCard.dataset.image) {
-    disableCards();
-    return;
-  }
-  unFlipCards();
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+  isMatch ? disableCards() : unFlipCards();
 }
 
 function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
+  resetBoard();
 }
 
 function unFlipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');  
+    secondCard.classList.remove('flip');
+    
+    resetBoard();
+  }, 1500);
+}
 
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function shuffle() {
+  cards.forEach(card => {
+    let random = Math.floor(Math.random() * 16);
+    card.style.order = random;
+  });
 }
 
  
